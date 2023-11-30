acb344e4d5e7bdf1049b61a1dccfbee2276c8499
aliflikescoding <alif.likes.coding@gmail.com>
2023-11-30 16:27:09 +0700

    Change code based on test

--- capitalize.js
+++ capitalize.js
@@ -1,6 +1,21 @@
-const capitalize = (string) => {
-  string[0].toUpperCase();
-  return string
+function isAlphabet(c) {
+  return /[a-zA-Z]/.test(c);
+}
+
+const capitalize = (str) => {
+  if (str == "") {
+    return "";
+  } else {
+    const arr = str.split("");
+    for (let i = 0; i < arr.length; i++) {
+      if (isAlphabet(arr[i])) {
+        arr[i] = arr[i].toUpperCase();
+        break;
+      }
+    }
+    const str2 = arr.join("");
+    return str2;
+  }
 };
 
 module.exports = capitalize;
